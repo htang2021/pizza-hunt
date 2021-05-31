00e3880f7bc79b1b90b8d1b97e0a8d571c64dacf
Hung Tang <hungtang@hotmail.com>
2021-05-31 11:33:11 -0700

    added validations in comment, pizza and reply schemas

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -29,7 +29,7 @@ const commentController = {
         Comment.findByIdAndUpdate(
             { _id: params.commentId },
             { $push: { replies: body } },
-            { new: true })
+            { new: true, runValidators: true })
             .then(dbPizzaData => {
                 if (!dbPizzaData) {
                     res.status(404).json({ message: 'No pizza found with this id!'});
